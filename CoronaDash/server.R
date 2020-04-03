cb6c03af25bce992a4259639968e6c1232de9e6c
dominic.ventura19 <dominic.ventura19@ncf.edu>
2020-04-02 22:30:31 -0400

    Finished map and table

--- CoronaDash/server.R
+++ CoronaDash/server.R
@@ -2,9 +2,8 @@ library(shiny)
 library(shinydashboard)
 library(ggplot2)
 library(readr)
-library(evaluate)
 library(maps)
-library(plotly)
+library(mapproj)
 library(DT)
 library(tidyverse)
 
@@ -18,10 +17,11 @@ shinyServer(function(input, output, session) {
         filePath = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv',
         readFunc = read_csv)
     
-    output$mydata <- renderDT({
+    output$mydata <- renderDataTable({
         df <- df()
-        corona <- df %>% filter(county != "Unknown")
-        corona <- df %>% filter(state == "Florida") %>% select(county, cases)
+        dta <- df %>% filter(state == "Florida")
+        corona <- dta %>% select(c(county, cases))
+        
         count <- aggregate(corona$cases, by=list(Category=corona$county), FUN=sum)
         colnames(count) <- c("County", "Count")
         count
@@ -29,32 +29,28 @@ shinyServer(function(input, output, session) {
     
     output$myplot <- renderPlot({
         df <- df()
-        corona <- df %>% filter(county != "Unknown")
-        corona <- df %>% filter(state == "Florida") %>% select(county,cases)
+        dta <- df
+        corona <- dta %>% filter(state == "Florida")
+        
         corona <- aggregate(corona$cases, by=list(Category=corona$county), FUN=sum)
         colnames(corona) <- c("County", "Count")
         
-        county_data <- map_data("county") %>% filter(region == "florida") 
-        corona$county <- tolower(corona$county)
+        
+        countydata <- map_data("county")
+        countydata1 <- countydata %>% filter(region == "florida")
+        countydata1$subregion <- tolower(countydata1$subregion)
+        corona$County <- tolower(corona$County)
 
-        county_data1 <- county_data %>% mutate(subregion = fct_recode(subregion, `st. johns` = "st johns", 
+        countydata <- countydata %>% mutate(subregion = fct_recode(subregion, `st. johns` = "st johns", 
                                                                       `st. lucie` = "st lucie", `desoto` = "de soto"))
        
-        coronaMap <- left_join(county_data1, corona, by = c(subregion = "county"))
+        coronaMap <- left_join(countydata1, corona, by = c(subregion = "County"))
         
         p <- ggplot(coronaMap, aes(x = long, y = lat, group = group, fill = Count)) + 
             geom_polygon(color = "black", size = 0.1) + theme_minimal() +
-            labs(x = "Longitude", y = "Latitude", fill = "Count")
+            labs(x = "Longitude", y = "Latitude", fill = "Count") + 
+            coord_map(projection = "albers", lat0 = 25, lat1 = 31)
         return(p)
     })
     
-    output$nrows <- renderValueBox({
-        nr <- nrow(df())
-        valueBox(
-            value = nr,
-            subtitle = "Number of Rows",
-            icon = icon("table"),
-            color = if (nr <=6) "yellow" else "aqua"
-        )
-    })
 })
