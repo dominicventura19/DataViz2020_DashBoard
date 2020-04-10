b75f655a8373ba4c044a39ed92024f0459550f2d
dominic.ventura19 <dominic.ventura19@ncf.edu>
2020-04-10 14:37:24 -0400

    Finshed.

--- Earthquakes/ui.R
+++ Earthquakes/ui.R
@@ -26,7 +26,7 @@ dashboardPage(
                   ),
                   conditionalPanel(
                     condition = "input.options == 'Date' ",
-                    selectInput("month", label = "Enter a month and day", choices = list(January = 1, February = 2, March = 3,
+                    selectInput("month", label = "Choose a month", choices = list(January = 1, February = 2, March = 3,
                                                                                         April = 4, May = 5, June = 6, July = 7,
                                                                                         August = 8, Sepetember = 9, October = 10,
                                                                                         November = 11, December = 12)),
