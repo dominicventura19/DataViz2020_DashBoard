2bd229e1987944fa0bd6dd8ddc294af42fda9834
dominic.ventura19 <dominic.ventura19@ncf.edu>
2020-04-10 13:41:39 -0400

    Finshed both tabs for app.

--- Earthquakes/server.R
+++ Earthquakes/server.R
@@ -6,6 +6,8 @@ library(maps)
 library(mapproj)
 library(DT)
 library(tidyverse)
+library(lubridate)
+library(plotly)
 
 earthquakes <- read_csv("earthquakes.csv")
 
@@ -20,7 +22,7 @@ shinyServer(function(input, output, session) {
   output$mydata <- renderDataTable({
     df <- df()
     dta <- df %>% group_by(country) %>% count() %>% na.omit() %>% arrange(desc(n))
-    colnames(dta) <- c("Country", "# of Earthquakes")
+    colnames(dta) <- c("Country/State", "# of Earthquakes")
     dta
   
   })
@@ -28,10 +30,59 @@ shinyServer(function(input, output, session) {
   output$myplot <- renderPlot({
     world <- map_data('world')
     
-    p <- ggplot() + geom_map(data = world, map = world, aes(x = long, y=lat, group=group, map_id=region), fill="white", colour="#7f7f7f", size=0.5) + 
-      geom_point(data = earthquakes, aes(x=longitude, y = latitude, colour = mag)) + 
-      scale_colour_gradient(low = "green",high = "magenta") 
-    return(p)
+    if (input$options == "All") {
+      allQuakes <- ggplot() + geom_map(data = world, map = world, aes(x = long, y=lat, group=group, map_id=region), 
+                                       fill="gray", colour="#7f7f7f", size=0.5) + 
+        theme_minimal() + 
+        geom_point(data = earthquakes, aes(x=longitude, y = latitude, color = mag)) +
+        scale_colour_gradient(low = "darkgreen", high = "red") 
+      return(allQuakes)
+      
+    }
+    
+    if (input$options == "Specific"){
+      
+      c <- sort(unique(earthquakes$country)) %>% na.omit
+      
+      eq.filtered <- earthquakes %>% filter(country == req(input$specific))
+      
+      p <- ggplot() + geom_map(data = world, map = world, aes(x = long, y=lat, group=group, map_id=region), 
+                               fill="gray", colour="#7f7f7f", size=0.5) + 
+        theme_minimal() + 
+        geom_point(data = eq.filtered, aes(x=longitude, y = latitude, color = mag, size = mag)) + 
+        scale_size(guide = 'none') +
+        scale_colour_gradient(low = "darkgreen", high = "magenta") 
+      return(p)
+      
+    }
+    
+    if (input$options == "Date") {
+  
+      dateQuakes <- earthquakes %>% filter(month(as.Date(time)) == input$month) %>% 
+        filter(day(as.Date(time)) == input$day)
+      
+      QuakesByDate <- ggplot() + geom_map(data = world, map = world, aes(x = long, y=lat, group=group, map_id=region), 
+                                          fill="gray", colour="#7f7f7f", size=0.5) + 
+        theme_minimal() +
+        geom_point(data = dateQuakes, aes(x=longitude, y = latitude, color = mag, size = mag)) + 
+        scale_size(guide = 'none') +
+        scale_colour_gradient(low = "darkgreen", high = "magenta") 
+      return(QuakesByDate)
+    }
+    
+  })
+  
+  output$myTimeSeries <- renderPlotly({
+    
+    TSEarthquakes <- earthquakes %>% select(time, country) %>% filter(country == input$countriesTS) %>% 
+                    group_by(year(as.Date(time))) %>% count() %>% ungroup() %>% mutate(new = cumsum(n))
+    
+    colnames(TSEarthquakes) <- c("Year", "Quakes in year", "Overall")
+    
+    
+    TSeries <- ggplot(TSEarthquakes, aes(x=Year, y=Overall)) + geom_point(colour = "red", size = 2) + geom_line()
+    return(ggplotly(TSeries))
+    
   })
   
 })
--- Earthquakes/ui.R
+++ Earthquakes/ui.R
@@ -1,34 +1,103 @@
 library(shiny)
-
+library(tidyverse)
 library(shiny)
 library(shinydashboard)
 library(DT)
+library(plotly)
 
 dashboardPage(
   dashboardHeader(title = "My Dashboard"),
-  dashboardSidebar(disable = T),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Map and Table", tabName = "MnT", icon = icon("globe-americas")),
+      menuItem("Time Series", tabName = "series", icon = icon("clock"))
+    )
+  ),
   dashboardBody(
-    # Boxes need to be put in a row (or column)
-    fluidRow(
-      box(width=6, 
-          status="info", 
-          title="Earthquakes of 7+ Magnitude",
-          solidHeader = TRUE,
-          plotOutput("myplot")
+    tabItems(
+      tabItem(tabName = "MnT",
+              fluidRow(
+                box(
+                  selectInput("options", "Plot every earthquake, ones from specific countries/states, or by date?", 
+                              choices = list(All = "All", Specific = "Specific", Date = "Date")),
+                  conditionalPanel(
+                    condition = "input.options == 'Specific' ",
+                    selectInput("specific", label = "Countries", choices = sort(c), multiple = T, selected = "")
+                  ),
+                  conditionalPanel(
+                    condition = "input.options == 'Date' ",
+                    selectInput("month", label = "Enter a month and day", choices = list(January = 1, February = 2, March = 3,
+                                                                                        April = 4, May = 5, June = 6, July = 7,
+                                                                                        August = 8, Sepetember = 9, October = 10,
+                                                                                        November = 11, December = 12)),
+                    #sliderInput("year", label = "Choose a year", min = 1900, max = 2020)
+                    sliderInput("day", label = "Choose a day", min = 1, max = 31, value = 1)
+                  )
+                  
+                ),
+                  
+                br(),
+                br(),
+                br(),
+                br(),
+                br(),
+                br(),
+                br(),
+                br(),
+                br(),
+                br(),
+                br(),
+                br(),
+                br(),
+                br(),
+                br(),
+                br(),
+               
+                box(width=6, 
+                    status="info", 
+                    title="Earthquakes of 7+ Magnitude from 1900 to 2020",
+                    solidHeader = TRUE,
+                    plotOutput("myplot")
+                ),
+                box(width=6, 
+                    status="warning", 
+                    title = "Data Frame",
+                    solidHeader = TRUE, 
+                    collapsible = TRUE, 
+                    footer="Read Remotely from File",
+                    dataTableOutput("mydata")
+                )
+              ),
+              ## Add some more info boxes
+              fluidRow(
+                valueBoxOutput(width=6, "nrows"),
+                infoBoxOutput(width=6, "ncol")
+              )
       ),
-      box(width=6, 
-          status="warning", 
-          title = "Data Frame",
-          solidHeader = TRUE, 
-          collapsible = TRUE, 
-          footer="Read Remotely from File",
-          dataTableOutput("mydata")
+      
+      tabItem(tabName = "series",
+              fluidRow(
+                box(
+                  selectInput("countriesTS", label = "Choose a country/state", choices = sort(c))
+                ),
+                br(),
+                br(),
+                br(),
+                br(),
+                br(),
+                br(),
+                br(),
+                
+                box(
+                  width=6,
+                  status="warning",
+                  title = "Earthquakes of 7+ Magnitude from 1900 to 2020",
+                  solidHeader = T,
+                  plotlyOutput("myTimeSeries")
+                )
+              )
+              
       )
-    ),
-    ## Add some more info boxes
-    fluidRow(
-      valueBoxOutput(width=6, "nrows"),
-      infoBoxOutput(width=6, "ncol")
     )
   )
 )
